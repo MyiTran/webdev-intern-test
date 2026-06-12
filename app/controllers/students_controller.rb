b2c3e6a6d1529cd570b84484300cb33125228ad3
My Tra <khoa@192.168.2.131>
2026-06-12 17:52:44 +0700

    Fix issues

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,6 @@
 class StudentsController < ApplicationController
   def index
+    @student = Student.find_by(registration_number: params[:registration_number]) if params[:registration_number].present?
+    @subjects = Subject.all
   end
 end
